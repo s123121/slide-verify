508d1f3d21aa7c4103b040b73bacd12895dc8d25
Hien Lam <hien@fiahub.com>
2022-12-15 13:43:47 +0700

    Fix mobile style scale 2

--- src/config.ts
+++ src/config.ts
@@ -3,37 +3,6 @@ let l = 42, // 滑块边长
   svCanvasWidth = 310, // canvas宽度
   svCanvasHeight = 210, // canvas高度
   L = l + r * 2 + 9; // 滑块实际边长
-const mobile = checkMobile();
-function calcPixel() {
-  if (mobile) {
-    svCanvasWidth = scalePixel(svCanvasWidth);
-    svCanvasHeight = scalePixel(svCanvasHeight);
-    l = scalePixel(l);
-    r = scalePixel(r);
-    L = scalePixel(L);
-  }
-}
-
-calcPixel();
-
-function scalePixel(pixel: number) {
-  // return pixel * window.devicePixelRatio / 1.5
-  return pixel * 2;
-}
-
-export function checkMobile(): boolean {
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    // true for mobile device
-    return true;
-  } else {
-    // false for not mobile device
-    return false;
-  }
-}
 
 export {
   l, // 滑块边长
@@ -41,5 +10,4 @@ export {
   svCanvasWidth, // canvas宽度
   svCanvasHeight, // canvas高度
   L, // 滑块实际边长
-  mobile,
 };
